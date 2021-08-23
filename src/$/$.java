879d25add6aa589b2c94226fb2890cfa7f7b999d
daanta-real <84055731+daanta-real@users.noreply.github.com>
2021-08-23 20:33:43 +0900

    Directly uploaded via Github website

---
+++ src/$/$.java
@@ -0,0 +1,8 @@
+package $;
+
+public class $ {
+	public static void pr (String s)                       { System.out.print(s);             }
+	public static void pn ()                               { System.out.println();            }
+	public static void pn (String s)                       { System.out.println(s);           }
+	public static void pf (String format, Object ... args) { System.out.printf(format, args); }
+}
