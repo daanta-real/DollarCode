220713e9580d141b4b087929d0f80a8d154ad31e
Daanta <daanta@naver.com>
2021-09-04 23:45:36 +0900

    210904_2345

--- $/$.java
+++ $/$.java
@@ -11,5 +11,19 @@ public class $ {
 
 	// printf
 	public static void pf (String format, Object ... args) { System.out.printf(format, args); }
-
+	
+	// 구글 번역 안내 달기 스트링
+	public static final String getGoogleTR_STR = "<style type=\"text/css\">\r\n"
+		+ "<!--\r\n"
+		+ "#goog-gt-tt {display:none !important;}\r\n"
+		+ ".goog-te-banner-frame {display:none !important;}\r\n"
+		+ ".goog-te-menu-value:hover {text-decoration:none !important;}\r\n"
+		+ ".goog-te-gadget-icon {background-image:url(//gtranslate.net/flags/gt_logo_19x19.gif) !important;background-position:0 0 !important;}\r\n"
+		+ "body {top:0 !important;}\r\n"
+		+ "-->\r\n"
+		+ "</style>\r\n"
+		+ "<div id=\"google_translate_element\"></div>\r\n"
+		+ "<script type=\"text/javascript\">\r\n"
+		+ "function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: 'ko', layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, includedLanguages: ''}, 'google_translate_element');}\r\n"
+		+ "</script><script type=\"text/javascript\" src=\"https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
 }
