1131eea6096fdac9f91382dab90e4edd2eeab91d
Daanta <daanta@naver.com>
2021-09-04 23:46:37 +0900

    210904_2346

--- src/$/$.java
+++ src/$/$.java
@@ -14,4 +14,19 @@ public class $ {
 
 	// returns ①, ②, ③, ... as you input 0, 1, 2, ...
 	public static char n(int number) { return (char)(9312 + number); }
+
+	// 구글 번역 안내 달기 스트링
+	public static final String getGoogleTR_str = "<style type=\"text/css\">\r\n"
+		+ "<!--\r\n"
+		+ "#goog-gt-tt {display:none !important;}\r\n"
+		+ ".goog-te-banner-frame {display:none !important;}\r\n"
+		+ ".goog-te-menu-value:hover {text-decoration:none !important;}\r\n"
+		+ ".goog-te-gadget-icon {background-image:url(//gtranslate.net/flags/gt_logo_19x19.gif) !important;background-position:0 0 !important;}\r\n"
+		+ "body {top:0 !important;}\r\n"
+		+ "-->\r\n"
+		+ "</style>\r\n"
+		+ "<div id=\"google_translate_element\"></div>\r\n"
+		+ "<script type=\"text/javascript\">\r\n"
+		+ "function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: 'ko', layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, includedLanguages: ''}, 'google_translate_element');}\r\n"
+		+ "</script><script type=\"text/javascript\" src=\"https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
 }
