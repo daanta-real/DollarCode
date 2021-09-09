e155737e67ff8883a967f96e37e0cf317940679d
Daanta <daanta@naver.com>
2021-09-09 10:02:27 +0900

    210909_1002

--- src/$/$.java
+++ src/$/$.java
@@ -4,17 +4,24 @@ public class $ {
 
 	// print
 	public static void pr (Object o)                       { System.out.print(o);             }
-
 	// println
 	public static void pn ()                               { System.out.println();            }
 	public static void pn (Object o)                       { System.out.println(o);           }
-
 	// printf
 	public static void pf (String format, Object ... args) { System.out.printf(format, args); }
+
+	// print
+	public static void er (Object o)                       { System.err.print(o);             }
+	// println
+	public static void en ()                               { System.err.println();            }
+	public static void en (Object o)                       { System.err.println(o);           }
+	// printf
+	public static void ef (String format, Object ... args) { System.err.printf(format, args); }
 
 	// returns ①, ②, ③, ... as you input 0, 1, 2, ...
 	public static char n(int number) { return (char)(9312 + number); }
 
+	//
 	// 구글 번역 안내 달기 스트링
 	public static final String getGoogleTR_str = "<style type=\"text/css\">\r\n"
 		+ "<!--\r\n"
@@ -29,4 +36,6 @@ public class $ {
 		+ "<script type=\"text/javascript\">\r\n"
 		+ "function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: 'ko', layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, includedLanguages: ''}, 'google_translate_element');}\r\n"
 		+ "</script><script type=\"text/javascript\" src=\"https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit\"></script>";
+
+
 }
